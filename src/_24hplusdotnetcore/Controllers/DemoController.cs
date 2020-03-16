31ff8da42e237054972665810d23f016a1221771
Chuong Nguyen <cnguyen67@dxc.com>
2020-03-16 11:47:17 +0700

    update source code dotnetcore

--- src/_24hplusdotnetcore/Controllers/DemoController.cs
+++ src/_24hplusdotnetcore/Controllers/DemoController.cs
@@ -1,9 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using _24hplusdotnetcore.Models;
 using _24hplusdotnetcore.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace _24hplusdotnetcore.Controllers
@@ -20,8 +23,26 @@ namespace _24hplusdotnetcore.Controllers
         [HttpGet]
         [Route("api/demos")]
         public List<Demo> Get() => _demoService.Get();
+
         [HttpGet]
         [Route("api/demo/{id}")]
         public Demo GetDemoById(string id) => _demoService.Get(id);
+
+        [HttpPost]
+        [Route("api/demos")]
+        public ActionResult<Demo> Create(Demo demo)
+        {
+            try
+            {
+                var newdemo = _demoService.Create(demo);
+                return Ok(newdemo);
+            }
+            catch (System.Exception ex)
+            {
+                
+                return StatusCode(StatusCodes.Status500InternalServerError ,new ResponseMessage{status = "ERROR", message = ex.Message});
+            }
+            
+        }
     }
 } 
---
+++ src/_24hplusdotnetcore/Models/ResponseMessage.cs
@@ -0,0 +1,8 @@
+namespace _24hplusdotnetcore.Models
+{
+    public class ResponseMessage
+    {
+        public string status { get; set; }
+        public string message { get; set; }
+    }
+}
